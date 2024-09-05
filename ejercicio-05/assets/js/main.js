c5ef43c6edcca6acf7dc3989effabe01b7f8f138
Isaac0405 <00095123@uca.edu.sv>
2024-09-04 22:42:53 -0600

    tercer ejercicio

--- ejercicio-05/assets/js/main.js
+++ ejercicio-05/assets/js/main.js
@@ -1,13 +1,24 @@
-const searchNumber = () => {
-    
-}
+const searchNumber = (a) => {
+    let factorial=1;
+    for (let i = 1; i <= a; i++) {
+        factorial *= i;   
+    }
+    return factorial;
+};
 
 const requestNumber = () => {
-    
+    let a=parseFloat(prompt("Introduce un numero entero no negativo para calcular su factorial;"));
+    if (isNaN(a)||a<0) {
+        alert("Ingrese un numero valido");
+        return requestNumber();
+    }
+    return a;
 }
 
 const main = () => {
-    
-}
+    let a=requestNumber();
+    let result=searchNumber(a);
+    alert(`El factorial de ${a} es: ${result}`)
+};
 
 main();
